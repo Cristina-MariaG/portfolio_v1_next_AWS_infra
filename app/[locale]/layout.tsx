9318eba4d8aab8c05adfdc169ea8d2f5f3884aa5
Cristina-MariaG <arcusi_cristina@yahoo.com>
2025-05-23 17:18:32 +0200

    changes for adding internationalisation

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,29 @@
+// app/[locale]/layout.tsx
+
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+import { ReactElement } from 'react';
+import { I18nProviderClient } from '../../locales/client';
+
+export default async function RootLayout({ params, children }: { params: Promise<{ locale: string }>; children: ReactElement }) {
+  const { locale } = await params;
+
+  return (
+    <html lang='fr'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <I18nProviderClient locale={locale}>{children}</I18nProviderClient>
+      </body>
+    </html>
+  );
+}
---
+++ app/[locale]/page.tsx
@@ -0,0 +1,18 @@
+import LanguageSelector from '@/components/LanguageSelector';
+import { getI18n } from '../../locales/server';
+
+export default async function Home() {
+  const t = await getI18n();
+
+  return (
+    <div className='relative min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-background text-foreground'>
+      <div className='absolute top-8 right-8'>
+        <LanguageSelector />
+      </div>
+
+      <main className='flex flex-col items-center justify-center text-center gap-[15px] py-20 sm:py-50'>
+        <h1 className='font-bold text-2xl sm:text-3xl'>{t('hello')}</h1>
+      </main>
+    </div>
+  );
+}
