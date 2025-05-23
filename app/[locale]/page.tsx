c1b2d421c15e0752dea597d35ba6fd05b6a99e7b
Cristina-MariaG <arcusi_cristina@yahoo.com>
2025-05-23 20:37:57 +0200

    for black theme

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 import LanguageSelector from '@/components/LanguageSelector';
 import { getI18n } from '../../locales/server';
+import ThemeToggle from '@/components/ThemeToogle';
 
 export default async function Home() {
   const t = await getI18n();
@@ -12,6 +13,8 @@ export default async function Home() {
 
       <main className='flex flex-col items-center justify-center text-center gap-[15px] py-20 sm:py-50'>
         <h1 className='font-bold text-2xl sm:text-3xl'>{t('hello')}</h1>
+        <p>hey there! I am cristina ghinda, a web developer from roumanie. gfgff</p>
+        <ThemeToggle />
       </main>
     </div>
   );
