dcc22db9c1cc7bf67f78c157a63555c47680dfa5
Cristina-MariaG <arcusi_cristina@yahoo.com>
2025-05-24 16:47:00 +0200

    add Language selector and toogle theme and add css for transation arrow footer

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,21 +1,9 @@
-import LanguageSelector from '@/components/LanguageSelector';
-import { getI18n } from '../../locales/server';
-import ThemeToggle from '@/components/ThemeToogle';
+import LandingPage from '@/components/page/LandingPage';
 
 export default async function Home() {
-  const t = await getI18n();
-
   return (
-    <div className='relative min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-background text-foreground'>
-      <div className='absolute top-8 right-8'>
-        <LanguageSelector />
-      </div>
-
-      <main className='flex flex-col items-center justify-center text-center gap-[15px] py-20 sm:py-50'>
-        <h1 className='font-bold text-2xl sm:text-3xl'>{t('hello')}</h1>
-        <p>hey there! I am cristina ghinda, a web developer from roumanie. gfgff</p>
-        <ThemeToggle />
-      </main>
-    </div>
+    <main>
+      <LandingPage />
+    </main>
   );
 }
